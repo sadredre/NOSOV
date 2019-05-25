ec6eabdd34108958914bc0ef9bcb7dd288d3ad8b
sadredre <50610851+sadredre@users.noreply.github.com>
2019-05-25 12:02:30 +0300

    'added'

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-function hello() {
-  console.log('Hello, World!');
-}
-
-hello();
+const express = require('express')
+const app = express()
+app.get('*', (req, res) => {
+  res.send('Hello from Express.js!')
+})
+app.listen(3000)
