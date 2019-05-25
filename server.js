e150bc2070133c28b8ccde84865f6497a0d9a1f2
sadredre <50610851+sadredre@users.noreply.github.com>
2019-05-25 23:11:51 +0300

    changes

--- server.js
+++ server.js
@@ -10,22 +10,24 @@ const setWebhook = url =>
   axios.get(`https://api.telegram.org/bot${token}/setWebhook?url=${url}`);
 const sendMessage = (chatId, text) =>
   axios.get(
-    `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${text}`
+    `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${text}`,
   );
 const parseWeather = async date => {
   const {
-    window: { document }
+    window: { document },
   } = await JSDOM.fromURL('', {
     resources: 'usable',
-    runScripts: 'dangerously'
+    runScripts: 'dangerously',
   });
   const tabs = Array.from(document.querySelectorAll('.main'));
   const tab = tabs.filter(el =>
     el
       .querySelector('.day-link')
       .getAttribute('data-link')
-      .includes(date)
+      .includes(date),
+
   )[0];
+
   return tab ? tab.querySelector('.temperature').textContent : 'no info';
 };
 
@@ -35,11 +37,11 @@ app.post('/telegram', (req, res) => {
 
   const {
     text,
-    chat: { id }
+    chat: { id },
   } = req.body.message;
   parseWeather(text).then(
     weather => sendMessage(id, weather),
-    () => sendMessage(id, 'error')
+    () => sendMessage(id, 'error'),
   );
   res.send();
 });
