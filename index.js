98aaf04dcc159e02af920bd262caa19081514d65
sadredre <50610851+sadredre@users.noreply.github.com>
2019-05-25 12:10:07 +0300

    fixed now

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+
+const app = express();
 app.get('*', (req, res) => {
-  res.send('Hello from Express.js!')
+  res.send('Hello from Express.js!');
 })
-app.listen(3000)
+app.listen(3000);
