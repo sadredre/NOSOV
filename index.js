55b14c2a7d804b6b7b435b36805487b363bfb15e
sadredre <50610851+sadredre@users.noreply.github.com>
2019-05-25 16:23:09 +0300

    another fix

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-const express = require('express');
 const axios = require('axios');
+const http = require('http');
 const { JSDOM } = require('jsdom');
 
-
-const setWebhook = url => axios.get(`https://api.telegram.org/bot${'827501344:AAG6sRKaMwJB3VPa-eGADe4BwzJfWUYBn4g'}/setWebhook?url=${url}`);
 const sendMessage = (chatId, text) => axios.get(`https://api.telegram.org/bot${'827501344:AAG6sRKaMwJB3VPa-eGADe4BwzJfWUYBn4g'}/sendMessage?chat_id=${chatId}&text=${text}`);
 const parseWeather = async (date) => {
-  const { window: { document } } = await JSDOM.fromURL('', { resources: 'usable', runScripts: 'dangerously' });
+  const { window: { document } } = await JSDOM.fromURL('', {
+    resources: 'usable',
+    runScripts: 'dangerously',
+  });
   const tabs = Array.from(document.querySelectorAll('.main'));
-  const tab = tabs.filter(el => el.querySelector('.day-link').getAttribute('data-link').includes(date))[0];
+  const tab = tabs.filter(el => el.querySelector('.day-link')
+    .getAttribute('data-link')
+    .includes(date))[0];
   return tab ? tab.querySelector('.temperature').textContent : 'no info';
 };
 
-const app = express();
-app.post('/telegram', (req, res) => {
+http.createServer((req, res) => {
   const { text, chat: { id } } = req.body.message;
-  parseWeather(text).then(
-    weather => sendMessage(id, weather),
-    () => sendMessage(id, 'error'),
-  );
+  parseWeather(text)
+    .then(
+      weather => sendMessage(id, weather),
+      () => sendMessage(id, 'error'),
+    );
   res.send();
-});
-app.get('*', (req, res) => {
-  res.send('Hello from Express.js!');
-});
-app.listen(3000);
-setWebhook(`${process.env.NOW_URL}/telegram`);
+})
+  .listen(8080);
